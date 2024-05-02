6c7320f2ee494a23cd8d4bc238859e8a924cac93
Ahocev <Ahocev@gmail.com>
2024-05-02 19:12:56 -0400

    The rest of the assignments

---
+++ java-classwork/Java Classwork/src/main/java/org/example/GLAB_303_3_1/SplitMethodDemo.java
@@ -0,0 +1,14 @@
+package org.example.GLAB_303_3_1;
+import java.util.Arrays;
+public class SplitMethodDemo {
+    public static void main(String[] args) {
+        String vowels = "a::b::c::d:e";
+
+        // splitting the string at "::"
+        // storing the result in an array of strings
+        String[] result = vowels.split("::");
+
+        // converting array to string and printing it
+        System.out.println("result = " + Arrays.toString(result));
+    }
+}
