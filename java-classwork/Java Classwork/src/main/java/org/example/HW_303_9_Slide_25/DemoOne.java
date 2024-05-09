7f502993c2653b5970591ecef17111b68461bf80
Ahocev <Ahocev@gmail.com>
2024-05-09 08:32:34 -0400

    no message

--- java-classwork/Java Classwork/src/main/java/org/example/HW_303_9_Slide_25/DemoOne.java
+++ java-classwork/Java Classwork/src/main/java/org/example/HW_303_9_Slide_25/DemoOne.java
@@ -1,2 +1,9 @@
-package org.example.HW_303_9_Slide_25;public class DemoOne {
+package org.example.HW_303_9_Slide_25;
+
+public class DemoOne {
+
+    static int var1 = 20;
+    static String var2 = "perscholas";
+    int var3 = 100;
+
 }
